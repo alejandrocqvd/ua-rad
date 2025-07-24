cf7fb0895f6990ccb3fe12fff0fcfc6fa87e7286
alejandrocqvd <alejandrocqvd@gmail.com>
2025-07-23 21:08:15 -0600

    started nav

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,5 @@
-import Image from "next/image";
+import HomePage from '@/features/home/HomePage';
 
 export default function Home() {
-  return (
-    <main className="">
-      
-    </main>
-  );
+  return <HomePage />;
 }
--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -1,5 +1,37 @@
+import Image from "next/image";
+import Link from "next/link";
+
 export const Navbar = () => {
     return (
-        <div>Navbar</div>
+        <nav className="flex flex-row justify-between items-center rounded-xl p-2 bg-[#247F4A] max-w-5xl h-14">
+            <Image
+                src="/branding/UARAD_LOGO_LIGHT.svg"
+                alt="UARAD Logo"
+                width={988}
+                height={294}
+                priority
+                className="h-6 w-auto"
+            />
+            <div className="text-white flex flex-row gap-8 justify-center items-center">
+                <Link href={'/'}>
+                    Home
+                </Link>  
+                <Link href={'/'}>
+                    Team
+                </Link>  
+                <Link href={'/'}>
+                    Competition
+                </Link>  
+                <Link href={'/'}>
+                    Outreach
+                </Link>  
+                <Link href={'/'}>
+                    Contact
+                </Link>  
+            </div>
+            <div className="bg-white rounded-xl p-4 text-[#247F4A] font-bold h-10 flex justify-center items-center">
+                Sponsor Us
+            </div>
+        </nav>
     );
 }
