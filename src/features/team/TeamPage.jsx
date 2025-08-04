5e36c79559edd1251e72e5c5c8212fe573dad34b
jlhuelar <huelarjl18@gmail.com>
2025-08-03 22:11:23 -0600

    teampage mobile

--- src/features/team/TeamPage.jsx
+++ src/features/team/TeamPage.jsx
@@ -26,15 +26,15 @@ const TeamPage = () => {
     return (
         <div className="relative min-h-screen w-full overflow-hidden bg-white">
           <div id="particles-js" className="absolute top-0 left-0 w-full h-full z-0"></div>
-            <div className="flex justify-center items-center pb-10 pt-30 relative z-10 h-screen">
-                <div className="bg-[url('/teampage/horse.jpg')] bg-cover bg-center w-full max-w-6xl h-150 flex justify-center items-center rounded-2xl"> </div>
+            <div className="flex justify-center items-center pb-10 pt-20 relative z-10 md:pt-30 h-screen md:h-screen">
+                <div className="bg-[url('/home/aboutUS.png')] bg-cover bg-center w-11/12 max-w-6xl h-200 flex justify-center items-center rounded-2xl md:w-2/3"> </div>
             </div>
             <div className="flex justify-center items-center mt-10 relative z-10">
-                <div className="bg-white border-5 border-[#247F4A] w-2/3 flex flex-col justify-center items-center rounded-2xl">
-                    <div className="h-20 content-normal text-4xl mt-5">
+                <div className="bg-white border-4 md:border-5 border-[#247F4A] w-11/12 md:w-2/3 flex flex-col justify-center items-center rounded-2xl">
+                    <div className="h-auto md:h-20 content-normal text-2xl md:text-4xl mt-5 px-4 text-center">
                         Meet The UA-RAD 2025 Team
                     </div>
-                    <div className="text-center text-xl mx-8 mb-5">
+                    <div className="text-center text-base md:text-xl mx-4 md:mx-8 mb-5">
                         A multidisciplinary team of innovators merging mechanical, electrical, and software expertise to pioneer the future of FPV drones platforms at the University of Alberta.                    
                     </div>
                 </div>
@@ -104,8 +104,7 @@ const TeamPage = () => {
                 </div>
             </div>
         </div>
-        
-        
+
     );
 }
 
