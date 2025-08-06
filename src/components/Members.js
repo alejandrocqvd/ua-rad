12775892566024eca2f6d07d4993b83ae0c95caf
jlhuelar <huelarjl18@gmail.com>
2025-08-05 21:25:36 -0600

    some team members added, need more detail and fix for longer names

--- src/components/Members.js
+++ src/components/Members.js
@@ -2,8 +2,8 @@ export const executiveMembers = [{
         title: "President",
         name: "Guneet Dhillon",
         bio: "Lorem ipsum dolor sit amet...",
-        photo: "/teampage/test.png",
-        linkedin: "https://linkedin.com/example1"
+        photo: "/teampage/memberphotos/guneetdhillon.jpg",
+        linkedin: "https://www.linkedin.com/in/gundhil/"
     },
     {
         title: "President",
@@ -33,28 +33,34 @@ export const executiveMembers = [{
 export const mechanicalMembers = [
     {
         title: "President",
-        name: "Guneet Dhillon",
+        name: "Tobi Aro",
         bio: "Lorem ipsum dolor sit amet...",
-        photo: "/teampage/test.png",
-        linkedin: "https://linkedin.com/example1"
+        photo: "/teampage/memberphotos/tobiaro.jpg",
+        linkedin: "https://www.linkedin.com/in/tobi-aro-2616822ab/?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"
     },
     {
         title: "Vice President",
-        name: "Jane Doe",
+        name: "Mitchell Ramsahoye",
         bio: "Sed do eiusmod tempor...",
-        photo: "/teampage/test2.png",
-        linkedin: "https://linkedin.com/example2"
+        photo: "/teampage/memberphotos/mitchellramsahoye.jpg",
+        linkedin: "https://www.linkedin.com/in/mitchell-ramsahoye-0983a82a7/?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app"
     },
-    // Add more members as needed
+    {
+        title: "Vice President",
+        name: "Mathew Gaddi",
+        bio: "Sed do eiusmod tempor...",
+        photo: "/teampage/memberphotos/mathewgaddi.jpg",
+        linkedin: "https://www.linkedin.com/in/mathew-gaddi-75a72a25a/?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app"
+    },
 ];
 
 export const electricalMembers = [
     {
         title: "President",
-        name: "Guneet Dhillon",
+        name: "Mark McKay",
         bio: "Lorem ipsum dolor sit amet...",
-        photo: "/teampage/test.png",
-        linkedin: "https://linkedin.com/example1"
+        photo: "/teampage/memberphotos/markmckay.jpg",
+        linkedin: "https://www.linkedin.com/in/mark-mckay-mm/?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app"
     },
     
     // Add more members as needed
@@ -63,24 +69,23 @@ export const electricalMembers = [
 export const softwareMembers = [
     {
         title: "President",
-        name: "Guneet Dhillon",
+        name: "Radhe Patel",
         bio: "Lorem ipsum dolor sit amet...",
-        photo: "/teampage/test.png",
-        linkedin: "https://linkedin.com/example1"
+        photo: "/teampage/memberphotos/radhepatel.jpg",
+        linkedin: "https://www.linkedin.com/in/radhe04/?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app"
+    },
+    {
+        name: "Tegen Hilker Readman",
+        bio: "Sed do eiusmod tempor...",
+        photo: "/teampage/memberphotos/tegenhilkerreadman.png",
+        linkedin: "https://www.linkedin.com/in/tegen-hilker-readman/"
     },
     {
         title: "Vice President",
-        name: "Jane Doe",
+        name: "Jacob Feng",
         bio: "Sed do eiusmod tempor...",
-        photo: "/teampage/test2.png",
-        linkedin: "https://linkedin.com/example2"
-    },
-    {
-        title: "Vice President",
-        name: "Jane Doe",
-        bio: "Sed do eiusmod tempor...",
-        photo: "/teampage/test2.png",
-        linkedin: "https://linkedin.com/example2"
+        photo: "/teampage/memberphotos/jacobfeng.jpg",
+        linkedin: "https://www.linkedin.com/in/jacobsolives/"
     },
     {
         title: "Vice President",
